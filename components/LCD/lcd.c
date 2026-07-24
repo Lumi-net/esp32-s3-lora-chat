dc3fcb4bbf96fe063360ac4a10f69464013285bb
zhentouyu <111236766+zhentouyu@users.noreply.github.com>
2026-07-24 23:48:30 +0800

    调整LCD颜色

--- components/LCD/lcd.c
+++ components/LCD/lcd.c
@@ -20,8 +20,9 @@ void lcd_init(void)
 
     esp_lcd_panel_dev_config_t panel_config = {
         .reset_gpio_num = GPIO_NUM_14,
-        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_BGR,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
         .bits_per_pixel = 16,
+        .data_endian = LCD_RGB_DATA_ENDIAN_LITTLE,
     };
     // 为 ST7789 创建 LCD 面板句柄，并指定 SPI IO 设备句柄
     ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
@@ -29,7 +30,7 @@ void lcd_init(void)
     //  复位、初始化、颜色反转、开启显示
     ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
     ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
-    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, false));
     ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
 
     duty_set(512); // 设置背光亮度为 50%
